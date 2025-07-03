11804e5743068f9684ad69b2cdbb5323d007000a
Alex Hagen-Zanker <a.hagen-zanker@surrey.ac.uk>
2025-07-03 22:39:41 +0100

    Removing earlier pseudo-solutions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,6 @@
 import os
 import sys
 import pytest
-
-# On Windows, with Python 3.8+, we need to add the vcpkg bin directory
-# to the DLL search path *before* osgeo.gdal is imported. This ensures that
-# the Python bindings load the same GDAL DLLs that the C++ plugin was
-# linked against.
-if sys.platform == "win32" and sys.version_info >= (3, 8):
-    vcpkg_gdal_bin_dir = os.getenv("VCPKG_GDAL_BIN_DIR")
-    if vcpkg_gdal_bin_dir and os.path.isdir(vcpkg_gdal_bin_dir):
-        print(f"Adding to DLL search path: {vcpkg_gdal_bin_dir}")
-        os.add_dll_directory(vcpkg_gdal_bin_dir)
-    else:
-        # This is not a fatal error, but a warning, as the PATH might still be correct.
-        print("Warning: VCPKG_GDAL_BIN_DIR is not set or not a valid directory.")
-
-# Now that the DLL path is set, we can safely import gdal.
 from osgeo import gdal
 
 @pytest.fixture(scope="session", autouse=True)
